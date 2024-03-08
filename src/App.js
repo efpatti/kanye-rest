ae6044e93289feae89986b3f6522cb7930dd03e9
efpatti <enzo.patti@aluno.senai.br>
2024-03-08 11:43:11 -0300

    project added to portfolio

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ const App = () => {
             Obter Frase de Kanye West
           </button>
         </div>
-        <div className="mt-4 text-center text-3xl italic tracking-wide leading-8 text-white bg-black bg-opacity-50 w-1/3">
+        <div className="mt-4 text-center text-3xl italic tracking-wide leading-8 text-white bg-black bg-opacity-50 max-w-3xl">
           {fraseKanye && <p>{fraseKanye}</p>}
           {erro && <p>{erro}</p>}
         </div>
